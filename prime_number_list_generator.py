adb4dd1a16adfb7d293aed1f52cfdacf28e32b84
Matt-LeBlanc <90570660+Matt-LeBlanc@users.noreply.github.com>
2021-12-02 18:22:25 -0500

    Update prime_number_list_generator.py

--- prime_number_list_generator.py
+++ prime_number_list_generator.py
@@ -8,7 +8,8 @@ import time
 from curtsies.fmtfuncs import yellow, bold, cyan
 import concurrent.futures
 import pickle
-global loop_count, loop_time, num_of_cores, multiplier, core
+
+global loop_count, loop_time, num_of_cores, multiplier
 
 
 def is_prime(n):
@@ -26,18 +27,17 @@ def is_prime(n):
 
 def find_primes(n1, n2):
     loop = 0
-    time.sleep(0.5)
     primes = []
-    json_dict = {'Primes': primes}
+    time.sleep(0.1)
     for i in range(n1, n2):
         if is_prime(i):
             primes.append(i)
             loop += 1
-            time.sleep(0.0000000001)
+            time.sleep(0.00000000001)
             if loop % 10_000 == 0:
                 display_time()
-    with open(f'{os.getcwd()}/primes.json', 'a') as f:
-        json.dump(json_dict, f)
+    with open(f'{os.getcwd()}/prime_numbers.json', 'a') as f:
+        json.dump(primes, f)
 
 
 def display_time():
@@ -55,23 +55,29 @@ def init_primes(a_list, b_list, number):
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(find_primes, a_list, b_list, )
     loop_count -= 1
+    size = os.path.getsize(f'{os.getcwd()}/prime_numbers.json')
+    size = size / (1024 * 1024)
+    if size > 1024:
+        size = size / 1024
+        size = f'{size:.2f} GB'
+    else:
+        size = f'{size:.2f} MB'
     os.system('clear')
     print(f"    Current Date: {datetime.datetime.now().strftime('%m/%d/%Y')} \n")
     print(f'    Elapsed Time: {time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))} \n')
     print(f'    Loop Count: {loop_count} \n')
     print(f'    Loop Time: {time.strftime("%H:%M:%S", time.gmtime(time.time() - loop_time))}')
+    print(f'    Size of prime_numbers.json: {size}  \n')
     with open('number.p', 'wb') as f:
         pickle.dump(number, f)
-    time.sleep(6.0)
+    time.sleep(1)
     main()
 
 
 def main():
-    global loop_count, loop_time, num_of_cores, multiplier, core
+    global loop_count, loop_time, num_of_cores, multiplier
     while loop_count > 0:
-        core = 1
         loop_time = time.time()
-        os.system('clear')
         with open('number.p', 'rb') as f:
             number = pickle.load(f)
             number -= 1
@@ -89,17 +95,17 @@ if __name__ == '__main__':
     num_of_cores = 15
     core = 1
     multiplier = 1_000_000
-    loop_count = 50
+    loop_count = 100
     start_time = time.time()
     loop_time = time.time()
     os.system('clear')
     print(cyan(bold(f"""
     ____  _                 _   _   __  __ _    ___ _   _  __ _ _   _ 
-   
+
                         Prime Number List Generator
-                            
+
                         Date: {datetime.datetime.now().strftime('%m/%d/%Y')}
-                            
+
     ____  _      ___      ____ __ _    ___ _   _  __ _ _   _ ___ _   _                        
     """)))
     time.sleep(1.0)
